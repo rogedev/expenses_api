041fdf833cbf45fb74c35e74b4ff9775cc41c1e5
rogelioRivas <apteruno@gmail.com>
2024-08-03 17:21:53 -0600

    add mysql support

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/rogedev/expenses_api/cmd/api"
+	"github.com/rogedev/expenses_api/config"
+	"github.com/rogedev/expenses_api/db"
 )
 
 func main() {
-	server := api.NewApiServer(":8080", nil)
+
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Env.DBUser,
+		Passwd:               config.Env.DBPassword,
+		Addr:                 config.Env.DBAddress,
+		DBName:               config.Env.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewApiServer(fmt.Sprintf(":%s", config.Env.Port), db)
+
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
